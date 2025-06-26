6808e1a5cb656d89950ef2f0cffd9da1f83c2f5a
LucasBondeDeSouza <bondelucas77@gmail.com>
2025-06-26 13:43:37 -0300

    atualização

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import 'remixicon/fonts/remixicon.css';
+import Header from "./components/Header";
+
+export default () => {
+
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
---
+++ frontend/src/components/Header/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default () => {
+
+    return (
+        <header class="fixed left-0 right-0 bg-white z-50 px-4 py-2 flex items-center justify-between border-b">
+            <div class="">
+                <button>
+                    <i class="ri-menu-fill text-xl"></i>
+                </button>
+                <div class="">
+                    <i class="ri-youtube-fill text-3xl text-red-600"></i>
+                    <span>Youtube</span>
+                    <span>BR</span>
+                </div>
+            </div>
+        </header>
+    )
+}
