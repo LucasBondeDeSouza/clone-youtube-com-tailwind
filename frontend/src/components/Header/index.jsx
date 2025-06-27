edacec54558230d8755e897e01a37e488507f1b1
LucasBondeDeSouza <bondelucas77@gmail.com>
2025-06-27 14:52:44 -0300

    atualização

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default ({ handleClickSidebar }) => {
 
     return (
-        <header className="fixed left-0 right-0 bg-white z-50 px-4 py-2 flex items-center justify-between border-b border-gray-200">
+        <header className="fixed left-0 right-0 bg-white z-50 px-4 py-2 flex items-center justify-between">
             <div className="flex items-center gap-4">
                 <button className="p-2 hover:bg-gray-100 rounded-full cursor-pointer" onClick={handleClickSidebar}>
                     <i className="ri-menu-fill text-xl"></i>
@@ -16,32 +16,43 @@ export default ({ handleClickSidebar }) => {
                 </div>
             </div>
 
-            <form className="flex-1 max-w-2xl mx-4 flex">
+            <form className="flex-1 max-w-2xl mx-4 hidden md:flex">
                 <input 
                     type="text" 
                     placeholder="Pesquisar"
                     className="flex-1 px-4 py-2 border border-gray-300 rounded-l-full rounded-r-0"
                 />
 
-                <button className="px-6 py-4 bg-gray-50 hover:bg-gray-200 border border-l-0 border-gray-300 rounded-r-full cursor-pointer">
-                    <i className="ri-search-line"></i>
+                <button className="px-6 py-2 bg-gray-50 hover:bg-gray-200 border border-l-0 border-gray-300 rounded-r-full cursor-pointer">
+                    <i className="ri-search-line text-xl"></i>
                 </button>
 
                 <button className="ml-2 px-4 py-2 bg-gray-100 rounded-full hover:bg-gray-200 cursor-pointer">
-                    <i className="ri-mic-line"></i>
+                    <i className="ri-mic-line text-xl"></i>
                 </button>
             </form>
 
-            <div className="flex items-center gap-2">
-                <button className="p-2 hover:bg-gray-100 rounded-full">
-                    <i className="ri-video-add-line text-xl"></i>
+            <div className="flex items-center gap-1">
+                <button className="p-2 hover:bg-gray-100 rounded-full cursor-pointer">
+                    <i className="ri-search-line text-lg"></i>
                 </button>
 
-                <button className="p-2 hover:bg-gray-100 rounded-full">
+                <button className="p-2 hover:bg-gray-100 rounded-full cursor-pointer">
+                    <i className="ri-mic-line text-lg"></i>
+                </button>
+            </div>
+
+            <div className="flex items-center gap-2">
+                <button className="px-4 py-2 bg-gray-100 hover:bg-gray-200 rounded-full flex items-center gap-2 cursor-pointer">
+                    <i className="ri-add-large-line text-xl"></i>
+                    <span className="font-semibold">Criar</span>
+                </button>
+
+                <button className="p-2 hover:bg-gray-100 rounded-full cursor-pointer">
                     <i className="ri-notification-3-line text-xl"></i>
                 </button>
 
-                <button className="ml-2 w-8 h-8 bg-purple-600 rounded-full text-white text-semibold">
+                <button className="ml-2 w-8 h-8 bg-purple-600 rounded-full text-white text-semibold cursor-pointer">
                     L
                 </button>
             </div>
